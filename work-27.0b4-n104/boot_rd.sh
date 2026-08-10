e8a73785f331cff9afcfe9c9a44d2ec8e20c374a
ZomkaDEV <zomka@fard.ing>
2026-08-10 23:30:46 +0300

    Update the RD script to wait a bit more
    Takes longer now but it is more stable

--- work-27.0b4-n104/boot_rd.sh
+++ work-27.0b4-n104/boot_rd.sh
@@ -1,61 +1,70 @@
 #!/bin/zsh
-../tools/usbliter8ctl boot ./Ramdisk/iBSS.raw;
-sleep 3;
-irecovery -f Ramdisk/iBEC.img4;
-irecovery -c go;
+../tools/usbliter8ctl boot ./Ramdisk/iBSS.raw
+sleep 4
+echo ibec flash
+irecovery -f Ramdisk/iBEC.img4
+irecovery -c go
 
-sleep 2;
-irecovery -f Ramdisk/RestoreLogo.img4;
+sleep 2
+echo restore logo flash
+irecovery -f Ramdisk/RestoreLogo.img4
 irecovery -c "setpicture 0x1"
 irecovery -c "bgcolor 0 191 255"    # sky background color
 
-irecovery -f Ramdisk/ANE.img4;
-irecovery -c firmware;
+sleep 2
+echo ane flash
+irecovery -f Ramdisk/ANE.img4
+irecovery -c firmware
 
-irecovery -f Ramdisk/AOP.img4;
-irecovery -c firmware;
+sleep 2
+echo aop flash
+irecovery -f Ramdisk/AOP.img4
+irecovery -c firmware
 
-irecovery -f Ramdisk/AVE.img4;
-irecovery -c firmware;
+sleep 2
+echo ave flash
+irecovery -f Ramdisk/AVE.img4
+irecovery -c firmware
 
-sleep 1;
-irecovery -f Ramdisk/SPTM.img4;
-irecovery -c firmware;
+sleep 2
+echo sptm flash
+irecovery -f Ramdisk/SPTM.img4
+irecovery -c firmware
 
-sleep 3;
-irecovery -f Ramdisk/TXM.img4;
-irecovery -c firmware;
+sleep 5
+irecovery -f Ramdisk/TXM.img4
+irecovery -c firmware
 
-irecovery -f Ramdisk/GFX.img4;
-irecovery -c firmware;
+irecovery -f Ramdisk/GFX.img4
+irecovery -c firmware
 
-irecovery -f Ramdisk/ISP.img4;
-irecovery -c firmware;
+irecovery -f Ramdisk/ISP.img4
+irecovery -c firmware
 
-sleep 1;
-irecovery -f Ramdisk/PMP.img4;
-irecovery -c firmware;
+sleep 1
+irecovery -f Ramdisk/PMP.img4
+irecovery -c firmware
 
-irecovery -f Ramdisk/RestoreTrustCache.img4;
-irecovery -c firmware;
+irecovery -f Ramdisk/RestoreTrustCache.img4
+irecovery -c firmware
 
-irecovery -f Ramdisk/SIO.img4;
-irecovery -c firmware;
+irecovery -f Ramdisk/SIO.img4
+irecovery -c firmware
 
-irecovery -f Ramdisk/WCH.img4;
-irecovery -c firmware;
+irecovery -f Ramdisk/WCH.img4
+irecovery -c firmware
 
-irecovery -f Ramdisk/RestoreRamdisk.img4;
+irecovery -f Ramdisk/RestoreRamdisk.img4
 irecovery -c "getenv ramdisk-delay"
-irecovery -c ramdisk;
+irecovery -c ramdisk
 
-sleep 2;
-irecovery -f Ramdisk/DeviceTree.img4;
-irecovery -c devicetree;
+sleep 2
+irecovery -f Ramdisk/DeviceTree.img4
+irecovery -c devicetree
 
-irecovery -f Ramdisk/SEP.img4;
-irecovery -c rsepfirmware;
+irecovery -f Ramdisk/SEP.img4
+irecovery -c rsepfirmware
 
-irecovery -f Ramdisk/Kernelcache.img4;
-irecovery -c bootx;
-sleep 1;
+irecovery -f Ramdisk/Kernelcache.img4
+irecovery -c bootx
+sleep 1
